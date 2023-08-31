d6a3afbce2831c8a9b57d30fc1faf79742706008
sidxh <totallysiddhant@gmail.com>
2023-09-01 01:39:06 +0530

    Update CardsSet.tsx

--- src/components/CardsSet.tsx
+++ src/components/CardsSet.tsx
@@ -1,106 +1,78 @@
-// import React from 'react'
-
-// export default function CardsSet() {
-//   return (
-//     <div>
-//       Cardset
-//     </div>
-//   )
-// }
-
-'use client'
-import React from 'react'
-import Cards from './Cards'
-import { useParams } from 'next/navigation'
-import Image from 'next/image'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import axios from 'axios';
-import NoData from '../../public/no-data.png'
+import NoData from '../../public/no-data.png';
+import { useRouter } from 'next/router';
+import Cards from './Cards';
 
 const CardsSet = () => {
+  const router = useRouter();
+  const { category, skill } = router.query;
 
-    const { category, skill } = useParams();
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [data, setData] = useState([]);
-
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (typeof category === 'string') {
           const response = await axios.get(`https://connectlnm-backend.onrender.com/${category}`);
           setData(response.data);
-          setIsLoading(false);
-        } catch (error) {
-          console.error(error);
         }
-      };
-  
-      fetchData();
-    }, [category]);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    console.log(category)
-      console.log(data)
-
-  // data.map(x=> console.log(x.name))
-
-  // const specificData = data
+    fetchData();
+  }, [category]);
 
   const specificData = data.filter(
     (skillData) =>
+      typeof skillData.category === 'string' &&
+      typeof category === 'string' &&
       skillData.category.toLowerCase() === category.toLowerCase() &&
-      skillData.skill.toLowerCase() === skill.toLowerCase()
+      skillData.skill.toLowerCase() === (skill as string).toLowerCase()
   );
 
-
   if (specificData.length === 0) {
-    // Handle case when skill data is not found
     return (
       <div className='not-found'>
-            {/* <img id='no-data-img' src={require('../images/no-data.png')} alt='No Data Available' /> */}
-            <Image alt='' src={NoData} />
-
-            <div className='not-found-text'>
-            <h1>No Data Yet</h1>
-            <p>We are working on expanding and scaling our database which is currently facing some non-technical issues. Hopefully we&apos;ll get it resolved ASAP. Meanwhile you can check all of other domains! </p>
-            </div>
-
+        <Image alt='' src={NoData} />
+        <div className='not-found-text'>
+          <h1>No Data Yet</h1>
+          <p>
+            We are working on expanding and scaling our database which is currently facing some non-technical issues.
+            Hopefully we&apos;ll get it resolved ASAP. Meanwhile, you can check all of other domains!
+          </p>
+        </div>
       </div>
-    )
-    
-    
-    
-;
+    );
   }
 
   const firstSkill = specificData[0];
 
   return (
-<div className='text-center mb-[3rem] mx-0'>
-        <h1 className='text-[2rem] font-bold mb-[3rem]'>{firstSkill.skill}</h1>
-        <div className='grid grid-cols-3 gap-[2rem] w-[70vw] p-[2rem]'>
+    <div className='text-center mb-[3rem] mx-0'>
+      <h1 className='text-[2rem] font-bold mb-[3rem]'>{firstSkill.skill}</h1>
+      <div className='grid grid-cols-3 gap-[2rem] w-[70vw] p-[2rem]'>
         {specificData.map((specificData, index) => (
-            <>
-                <Cards 
-                name={specificData.name}
-                skill={specificData.skill}
-                skillset={specificData.skillset}
-                experience={specificData.experience}
-                achievement={specificData.achievement}
-                rollno={specificData.rollno}
-                linkedin={specificData.linkedin}
-                email={specificData.email}
-                />
-
-            </>        
-      ))}
-            
-        </div>
+          <Cards
+            key={index}
+            name={specificData.name}
+            skill={specificData.skill}
+            skillset={specificData.skillset}
+            experience={specificData.experience}
+            achievement={specificData.achievement}
+            rollno={specificData.rollno}
+            linkedin={specificData.linkedin}
+            email={specificData.email}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-
-export default CardsSet
+export default CardsSet;
