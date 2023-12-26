8faffee61997b404bcaf0f8de792fe024a63a766
sidxh <totallysiddhant@gmail.com>
2023-12-26 23:04:29 +0530

    navbar fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,13 @@ export default function Header() {
   return (
     <div>
       <header>
-        <nav className='mx-[2%] my-0 mb-[4rem] py-8 flex justify-start items-center text-white text-[1.1rem] gap-14 sm:block hidden'>
-            <Link className='ml-[5rem] my-0 mr-auto' href='/'><Image alt='' src={NameHead} className='w-52' /></Link>
+        <nav className='mx-[2%] my-0 mb-[4rem] py-8 flex justify-start items-center text-white sm:text-[1.1rem] text-[0.75rem] sm:gap-14 gap-6'>
+            <Link className='sm:ml-[5rem] ml-0 my-0 mr-auto' href='/'><Image alt='' src={NameHead} className='sm:w-52 w-[80%]' /></Link>
             <Link className='' href='/vision'>Vision</Link>
             <Link className='' href='/explore'>Explore</Link>
-            <Link className='' href='/opportunities'>Opportunities</Link>
+            <Link className='sm:block hidden' href='/opportunities'>Opportunities</Link>
             <Link className='mr-auto' href='/reach-out'>Reachout</Link>
-            <button className='mr-[5rem] w-48 px-2 py-4 rounded-lg bg-blue-200 font-bold text-black'><Link href='/reach-out'>Get Listed</Link></button>
+            <button className='mr-[5rem] w-48 px-2 py-4 rounded-lg bg-blue-200 font-bold text-black sm:block hidden'><Link href='/reach-out'>Get Listed</Link></button>
         </nav>
       </header>
     </div>
